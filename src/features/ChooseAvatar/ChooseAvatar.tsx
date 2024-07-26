53eefdb465e1ea572d05d54cd16385371acc457e
Kshiteej Jain <kshiteejjain@gmail.com>
2024-07-26 06:50:05 +0530

    Add files via upload

--- src/features/ChooseAvatar/ChooseAvatar.tsx
+++ src/features/ChooseAvatar/ChooseAvatar.tsx
@@ -1,67 +1,47 @@
 import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import { firestore } from '../../utils/Firebase';
+import { doc, getDoc } from 'firebase/firestore';
 
-import OriginalImage1 from '../../assets/originalimages/Lady_Bison.png';
-import OriginalImage2 from '../../assets/originalimages/Lady_Deer.png';
-import OriginalImage3 from '../../assets/originalimages/Lady_Deer2.png';
-import OriginalImage4 from '../../assets/originalimages/Lady_Leapord.png';
-import OriginalImage5 from '../../assets/originalimages/Lady_leopard2.png';
-import OriginalImage6 from '../../assets/originalimages/Lady_Leopard3.png';
-import OriginalImage7 from '../../assets/originalimages/Lady_SlothBear.png';
-import OriginalImage8 from '../../assets/originalimages/Lady_Tiger.png';
-import OriginalImage9 from '../../assets/originalimages/Man_Bison.png';
-import OriginalImage10 from '../../assets/originalimages/Man_Eagle.png';
-import OriginalImage11 from '../../assets/originalimages/Man_FlyCatcher.png';
-import OriginalImage12 from '../../assets/originalimages/Man_Leapord.png';
-import OriginalImage13 from '../../assets/originalimages/Man_SlothBear.png';
-import OriginalImage14 from '../../assets/originalimages/Man_Tiger.png';
-
-import lowResImage1 from '../../assets/lowresimages/Lady_Bison.png';
-import lowResImage2 from '../../assets/lowresimages/Lady_Deer.png';
-import lowResImage3 from '../../assets/lowresimages/Lady_Deer2.png';
-import lowResImage4 from '../../assets/lowresimages/Lady_Leapord.png';
-import lowResImage5 from '../../assets/lowresimages/Lady_leopard2.png';
-import lowResImage6 from '../../assets/lowresimages/Lady_Leopard3.png';
-import lowResImage7 from '../../assets/lowresimages/Lady_SlothBear.png';
-import lowResImage8 from '../../assets/lowresimages/Lady_Tiger.png';
-import lowResImage9 from '../../assets/lowresimages/Man_Bison.png';
-import lowResImage10 from '../../assets/lowresimages/Man_Eagle.png';
-import lowResImage11 from '../../assets/lowresimages/Man_FlyCatcher.png';
-import lowResImage12 from '../../assets/lowresimages/Man_Leapord.png';
-import lowResImage13 from '../../assets/lowresimages/Man_SlothBear.png';
-import lowResImage14 from '../../assets/lowresimages/Man_Tiger.png';
-
-const originalImages = [
-  OriginalImage1, OriginalImage2, OriginalImage3, OriginalImage4, OriginalImage5, OriginalImage6, OriginalImage7,
-  OriginalImage8, OriginalImage9, OriginalImage10, OriginalImage11, OriginalImage12, OriginalImage13, OriginalImage14
-];
-
-const lowResImages = [
-  lowResImage1, lowResImage2, lowResImage3, lowResImage4,
-  lowResImage5, lowResImage6, lowResImage7, lowResImage8,
-  lowResImage9, lowResImage10, lowResImage11, lowResImage12, lowResImage13, lowResImage14
-];
+type Props = {
+  gender: string,
+  imageName: string | number,
+  ageRange: string,
+  imageUrl: string,
+  url: string
+}
 
 const OriginalImages = () => {
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
-  const [imagePaths, setImagePaths] = useState<string[]>(originalImages);
+  const [imagePaths, setImagePaths] = useState<Props[]>([]);
+  const [filteredImages, setFilteredImages] = useState<Props[]>([]);
+  const [gender, setGender] = useState<string>('');
+  const [ageRange, setAgeRange] = useState<string>('');
+  const navigate = useNavigate();
 
   useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth <= 768) {
-        setImagePaths(lowResImages);
-      } else {
-        setImagePaths(originalImages);
+    const fetchImages = async () => {
+      try {
+        const userDocRef = doc(firestore, 'AtalBotanicalUsers', 'images');
+        const docSnap = await getDoc(userDocRef);
+        
+        if (docSnap.exists()) {
+          const data = docSnap.data();
+          setImagePaths(data.images || []);
+        } else {
+          console.log('No such document!');
+        }
+      } catch (error) {
+        console.error('Error fetching images:', error);
       }
     };
 
-    handleResize(); // Check on component mount
-    window.addEventListener('resize', handleResize); // Add event listener for window resize
-
-    return () => {
-      window.removeEventListener('resize', handleResize); // Clean up event listener on unmount
-    };
+    fetchImages();
   }, []);
+
+  console.log('filteredImages', filteredImages)
 
   const handleImageClick = (imagePath: string) => {
     setSelectedImage(imagePath);
@@ -78,39 +58,76 @@ const OriginalImages = () => {
     document.cookie = existingCookie;
   };
 
-  const handleContinue = () => {
+  const handleFilterChange = () => {
+    let filtered = imagePaths;
+    if (gender) {
+      filtered = filtered.filter(image => image.gender === gender);
+    }
+    if (ageRange) {
+      filtered = filtered.filter(image => image.ageRange === ageRange);
+    }
+    setFilteredImages(filtered);
+  };
+
+  const handleGenderChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setGender(e.target.value);
+  };
+
+  const handleAgeRangeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setAgeRange(e.target.value);
+  };
+
+  useEffect(() => {
+    handleFilterChange();
+  }, [gender, ageRange, imagePaths]);
+
+  const handleClick = () => {
     let natureHeroExisting = localStorage.getItem('natureHero');
     let natureHeroExistingObject = natureHeroExisting ? JSON.parse(natureHeroExisting) : {};
     natureHeroExistingObject.history = (natureHeroExistingObject.history || '') + 'ImgSlctd!';
     localStorage.setItem('natureHero', JSON.stringify(natureHeroExistingObject));
-    window.location.href = 'https://rzp.io/l/CK9bopcdc4'
-  }
+    navigate('/PaymentGateway');
+  };
 
   return (
     <>
       <Header />
       <div className='wrapper'>
         <h2>Select a photo / एक फोटो चुनें</h2>
+        <div className='filters'>
+          <h2>Filters</h2>
+          <select className='form-control' value={gender} onChange={handleGenderChange}>
+            <option value=''>All</option>
+            <option value='Male'>Male</option>
+            <option value='Female'>Female</option>
+          </select>
+          <select className='form-control' value={ageRange} onChange={handleAgeRangeChange}>
+            <option value=''>All</option>
+            <option value='5-15'>5-15 years</option>
+            <option value='16-30'>16-30 years</option>
+            <option value='30-50'>30-50 years</option>
+          </select>
+        </div>
         <div className='choose-avatar'>
-          {imagePaths.map((imagePath, index) => (
-            <div key={index}
-              className={selectedImage === imagePath ? 'remaker-image remaker-active' : 'remaker-image'}>
+          {filteredImages.map((image, index) => (
+            <div key={index} className={selectedImage === image.url ? 'remaker-image remaker-active' : 'remaker-image'}>
               <img
-                src={imagePath}
+                src={image.imageUrl}
                 alt={`Image ${index + 1}`}
-                onClick={() => handleImageClick(imagePath)}
+                onClick={() => handleImageClick(image.url)}
               />
             </div>
           ))}
         </div>
         {selectedImage && (
           <div className='cta-center'>
-            <button onClick={handleContinue}>Continue</button>
+            <button onClick={handleClick}>Continue</button>
           </div>
         )}
       </div>
+      <Footer />
     </>
   );
-}
+};
 
 export default OriginalImages;
